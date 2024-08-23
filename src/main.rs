7ff8537851a3587577a58194655f9e5163b70a46
Cameron Barnes <cameron_barnes@outlook.com>
2024-08-22 17:00:06 -0700

    added clap and thiserror as options, added tracing and added reasonable defaults for it and anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, Mom!");
+use anyhow::Result;
+use tracing::info;
+use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
+fn main() -> Result<()> {
+    tracing_subscriber::registry().with(fmt::layer()).init();
+
+    info!("Hello, Mom!");
+
+    Ok(())
 }
