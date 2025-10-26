3579443ab4ee48c53dbbb7f9e457b7e99d9d6a12
Cameron Barnes <cameron_barnes@outlook.com>
2025-10-26 16:39:41 -0700

    Upgraded rust version, added allow on main function to prevent error/warn on new project

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use anyhow::Result;
 use tracing::info;
 use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt};
 
+#[allow(clippy::unnecessary_wraps)]
 fn main() -> Result<()> {
     tracing_subscriber::registry().with(fmt::layer()).init();
 
